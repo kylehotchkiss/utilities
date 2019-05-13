9de7fc44857f90d860d51b83b774a92f87bc879a
Kyle Hotchkiss <kyle@kylehotchkiss.com>
2019-05-13 13:58:40 +0530

    add totally speculative am22 i130 estimates

--- 2019-05-13-uscis-application-status-estimates/index.js
+++ 2019-05-13-uscis-application-status-estimates/index.js
@@ -8,7 +8,7 @@ const moment = require('moment');
 const RECEIPT_DATE = moment.unix( process.env.RECEIPT_DATE );
 const DATE_FORMAT = `MMMM D, YYYY`;
 
-const getStatus = async () => {
+const getUSCISStatus = async () => {
     try {
         const { data: caseStatusData } = await axios.post('https://egov.uscis.gov/casestatus/mycasestatus.do', qs.stringify({
             appReceiptNum: process.env.RECEIPT_NUMBER,
@@ -25,8 +25,10 @@ const getStatus = async () => {
     }
 }
 
-const getEstimates = async () => {
+const getUSCISEstimates = async () => {
     try {
+        let spouse = {};
+
         const { 
             data: { 
                 data: { 
@@ -37,8 +39,6 @@ const getEstimates = async () => {
             }  
         } = await axios(`https://egov.uscis.gov/processing-times/api/processingtime/I-130/${ process.env.SERVICE_CENTER }`);
     
-        let spouse = {};
-        const receiptDate = process.env.RECEIPT_NUMBER
     
         for ( const type of processingTimeData ) {
             if ( type.form_type === '134A-IR' ) {
@@ -58,10 +58,10 @@ const getEstimates = async () => {
             .add(Math.floor(latestDuration), 'months')
             .add( ( (latestDuration % 1) * 30 ), 'days' );
     
-        console.log( 'Received', RECEIPT_DATE.format(DATE_FORMAT) );
-        console.log( 'Earliest Approval', earliest.format(DATE_FORMAT) );
-        console.log( 'Latest Approval', latest.format(DATE_FORMAT) );
-        console.log( 'Estimate data as of', spouse.publication_date );
+        console.log( 'USCIS Estimate data as of', spouse.publication_date );
+        console.log( 'USCIS Received', RECEIPT_DATE.format(DATE_FORMAT) );
+        console.log( 'USCIS Earliest Approval', earliest.format(DATE_FORMAT) );
+        console.log( 'USCIS Latest Approval', latest.format(DATE_FORMAT) );
 
         return true;
     } catch( error ) {
@@ -70,13 +70,30 @@ const getEstimates = async () => {
     }  
 }
 
+const getAM22Status = async () => {
+    try {
+        const { data: processingTimeHTML } = await axios('https://www.am22tech.com/uscis/current-i130-processing-time-green-card-for-parents/');
+
+        const $ = cheerio.load( processingTimeHTML );
+        // LOL
+        const response = $('#texas tbody tr').eq(1).find('td').eq(1).html().split('<br>')[0];
+        const processedDate = moment(response, 'DD MMM, YY');
+        const remainingTime = RECEIPT_DATE.diff(processedDate, 'days');
+        const bestEstimatedDate = moment( RECEIPT_DATE ).add(remainingTime, 'days');
+
+        console.log( 'AM22Tech Speculative Approval', bestEstimatedDate.format(DATE_FORMAT) );
+    } catch ( error ) {
+        console.error( error );
+        throw new Error( error );
+    }
+}
+
 const main = (async () => {
     try {
-        const status = await getStatus();
-        
-        console.log( status );
-
-        const estimates = await getEstimates();
+        //const status = await getUSCISStatus();        
+        //console.log( status );
+        const estimates = await getUSCISEstimates();
+        const am22estimate = await getAM22Status()
     } catch( error ) {
         console.error('Error encountered, closing');
     }
